28075968cd2f10259ee4f58166466e076132c8a9
Federicaye <gallofederica58@gmail.com>
2024-03-14 12:14:01 +0100

    snack 2

---
+++ script.js
@@ -0,0 +1,19 @@
+/* let numbers = [];
+
+
+let sum = 0;
+while (sum <50) {
+    const newNumber = parseInt(prompt("Enter a number"));
+    sum += newNumber;
+    numbers.push(newNumber);
+} */
+
+let number = parseInt(prompt("Enter a number"))
+
+function array () {
+    for (let i = 0; 1<number, i++){
+        
+    }
+     
+
+}
